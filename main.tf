88a05b7886d09a2b21b93dc09f8d177901fba1ee
edup92 <edup92mail@gmail.com>
2025-11-15 20:22:33 +0000

    up

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "null_resource" "run_ansible" {
         --user ubuntu \
         --private-key "${local_file.file_keypair.filename}" \
         --extra-vars "@${path.module}/vars.json" \
-        -o StrictHostKeyChecking=no \
+        --ssh-extra-args="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
         playbook.yml
     EOT
   }
