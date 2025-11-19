8cc8391a76928fb146c0b538cc0d421646ab680d
edup92 <edup92mail@gmail.com>
2025-11-19 18:40:04 +0000

    up

--- locals.tf
+++ locals.tf
@@ -2,10 +2,10 @@
 locals {
   # Instances
 
-  instance_vscode_name  = "${var.project_name}-instance-main"
-  disk_vscode_name       = "${var.project_name}-disk-main"
-  snapshot_vscode_name = "${var.project_name}-snapshot-main"
-  instancegroup_vscode_name = "${var.project_name}-instancegroup-main"
+  instance_bitwarden_name  = "${var.project_name}-instance-main"
+  disk_bitwarden_name       = "${var.project_name}-disk-main"
+  snapshot_bitwarden_name = "${var.project_name}-snapshot-main"
+  instancegroup_bitwarden_name = "${var.project_name}-instancegroup-main"
 
   # Secrets
 
@@ -18,13 +18,13 @@ locals {
   firewall_localssh_name = "${var.project_name}-firewall-localssh"
   firewall_tempssh_name = "${var.project_name}-firewall-tempssh"
   healthcheck_443_name = "${var.project_name}-healthcheck-https"
-  backend_vscode_name = "${var.project_name}-backend-main" 
-  cloudarmor_vscode_name = "${var.project_name}-cloudarmor-main"
-  urlmap_vscode_name = "${var.project_name}-urlmap-main"
-  lbip_vscode_name = "${var.project_name}-lbip-main"
-  lbtarget_vscode_name = "${var.project_name}-lbtarget-main"
-  lbrule_vscode_name = "${var.project_name}-lbrule-main"
-  ssl_vscode_name = "${var.project_name}-ssl-main"
+  backend_bitwarden_name = "${var.project_name}-backend-main" 
+  cloudarmor_bitwarden_name = "${var.project_name}-cloudarmor-main"
+  urlmap_bitwarden_name = "${var.project_name}-urlmap-main"
+  lbip_bitwarden_name = "${var.project_name}-lbip-main"
+  lbtarget_bitwarden_name = "${var.project_name}-lbtarget-main"
+  lbrule_bitwarden_name = "${var.project_name}-lbrule-main"
+  ssl_bitwarden_name = "${var.project_name}-ssl-main"
 
   # Oauth
 
